42b064d0bd0b8f7e1847a85e2965e7b49e89f357
mstout12 <mstout12@uncc.edu>
2020-10-27 12:27:30 -0400

    flask app updated templates and static

--- flask_app/flask01.py
+++ flask_app/flask01.py
@@ -3,6 +3,7 @@
 # imports
 import os                 # os is used to get environment variables IP & PORT
 from flask import Flask   # Flask is the web app that we will customize
+from flask import render_template
 
 app = Flask(__name__)     # create an app
 
@@ -12,11 +13,10 @@ app = Flask(__name__)     # create an app
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Welcome, Notes App User!'
+    return render_template('index.html')
 
 
-app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
-
+app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True);
 # To see the web page in your web browser, go to the url,
 #   http://127.0.0.1:5000
 
